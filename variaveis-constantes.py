f0735f30e53f0add99d3aa84ad073993525eb069
Cassiana <lima.cglima@gmail.com>
2022-10-05 15:31:15 -0300

    tipos de dados e variaveis e constantes

---
+++ variaveis-constantes.py
@@ -0,0 +1,13 @@
+nome = "Cassiana"
+idade = 32
+
+nome, idade = "Gustavo", 34
+
+print(nome, idade)
+
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "MG"]
+
+print(BRAZILIAN_STATES)
