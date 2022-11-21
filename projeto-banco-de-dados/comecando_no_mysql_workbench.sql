50a85663f2bd40a36f84130cb5249763cd499624
Cassiana <lima.cglima@gmail.com>
2022-11-21 14:56:24 -0300

    arquivos referentes ao modulo 3 do curso banco de dados

---
+++ projeto-banco-de-dados/comecando_no_mysql_workbench.sql
@@ -0,0 +1,50 @@
+show databases;
+create database if not exists first_example;
+use first_example;
+CREATE TABLE person(
+	person_id smallint unsigned,
+	fname varchar(20),
+	lname varchar(20),
+	gender enum('M','F'),
+	birth_date DATE,
+	street varchar(30),
+	city varchar(20),
+	state varchar(20),
+	country varchar(20),
+	postal_code varchar(20),
+    constraint pk_person primary key(person_id)
+);
+desc person;
+
+CREATE TABLE favorite_food(
+	person_id smallint unsigned,
+    food varchar(20),
+    constraint pk_favorite_food primary key (person_id, food),
+    constraint fk_favorite_food_person_id foreign key (person_id) 
+    references person(person_id)
+);
+
+desc favorite_food;
+show databases;
+desc information_schema.table_constraints;
+SELECT *  FROM information_schema.table_constraints
+WHERE constraint_schema = 'first_example';
+
+
+desc person;
+
+INSERT INTO person values ('2', 'Carolina', 'Silva', 'F', '1979-08-21',
+							'rua tal', 'Cidade J', 'RJ', 'Brasil', '26054-89'),
+                            ('3', 'Carolina', 'Silva', 'F', '1979-08-21',
+							'rua tal', 'Cidade J', 'RJ', 'Brasil', '26054-89');
+SELECT * FROM person;
+
+DELETE FROM person WHERE person_id=2;
+
+ DESC favorite_food;
+ 
+ INSERT INTO favorite_food VALUES (1, 'lasanha'), 
+									(5, 'carne assada'),
+                                    (6, 'fetuccine');
+									
+SELECT * FROM favorite_food;
